0e33fd966c854574b52325f377418b2295c21b0a
fk <francoisk777@gmail.com>
2014-09-14 20:31:21 +0200

    Misc. refactoring.

--- src/impl/server.c
+++ src/impl/server.c
@@ -37,14 +37,11 @@ struct context
 
 #pragma GCC diagnostic pop
 
-static void print_xfer(const struct xfer* x)
-{
-    printf("stat_fd: %d; dest_fd: %d; cmd: %d;"
-           " file->size: %ld; file->blksize: %d\n",
-           x->stat_fd, x->dest_fd, x->cmd, x->file.size, x->file.blksize);
-}
+static void print_xfer(const struct xfer* x);
 
-#define PRINT_XFER(x) printf("%s: ", __func__); print_xfer(x)
+#define PRINT_XFER(x)                           \
+    printf("%s: ", __func__);                   \
+    print_xfer(x)
 
 static bool errno_is_fatal(const int err);
 
@@ -52,14 +49,188 @@ static bool process_file_op(struct context* ctx, struct xfer* xfer);
 
 static void context_destruct(struct context* ctx);
 
-static void delete_xfer(struct xfer* x)
+static bool add_read_xfer(struct context* ctx,
+                          const char* filename,
+                          int dest_fd);
+
+static bool add_send_xfer(struct context* ctx,
+                          const char* filename,
+                          int stat_fd,
+                          int dest_fd);
+
+static bool send_stat(int fd,
+                      enum prot_stat stat,
+                      size_t file_size,
+                      off_t file_offset);
+
+static bool send_xfer_stat(const struct xfer* x, enum prot_stat stat);
+
+/*
+  Removing, e.g., xfer 'b':
+
+  |a|b|c|d| -> |a|d|c|
+
+  (i.e., the last element replaces the removed one and its index updated.)
+*/
+static void remove_xfer(struct context* ctx, struct xfer* xfer);
+
+static void delete_xfer(struct xfer* x);
+
+static bool process_events(struct context* ctx,
+                           const int nevents,
+                           uint8_t* buf, const size_t buf_size);
+
+static bool context_construct(struct context* ctx,
+                              int poll_timeout,
+                              int listenfd,
+                              const char* sockname,
+                              int maxfds);
+
+static void context_destruct(struct context* ctx);
+
+bool srv_run(const int listenfd, const char* sockname, const int maxfds)
 {
-    close(x->dest_fd);
-    if (x->stat_fd != x->dest_fd)
-        close(x->stat_fd);
-    file_close(&x->file);
-    free(x);
+    struct context ctx;
+
+    if (!context_construct(&ctx, 1000, listenfd, sockname, maxfds))
+        return false;
+
+    if (!syspoll_register(ctx.poller,
+                          ctx.listenfd,
+                          &ctx.listenfd,
+                          SYSPOLL_READ | SYSPOLL_WRITE)) {
+        goto fail;
+    }
+
+    uint8_t* const recvbuf = calloc(PROT_PDU_MAXSIZE, 1);
+    if (!recvbuf)
+        goto fail;
+
+    /* The processing loop */
+    /* for (;;) { */
+    for (int i = 0; i < 3; i++) {
+        const int n = syspoll_poll(ctx.poller);
+
+        if (!process_events(&ctx, n, recvbuf, PROT_PDU_MAXSIZE))
+            break;
+    }
+
+    free(recvbuf);
+    context_destruct(&ctx);
+
+    return true;
+
+ fail:
+    context_destruct(&ctx);
+    return false;
 }
+
+static bool recv_and_process_requests(struct context* ctx,
+                                      uint8_t* buf,
+                                      size_t buf_size);
+
+static bool process_events(struct context* ctx,
+                           const int nevents,
+                           uint8_t* buf, const size_t buf_size)
+{
+    for (int i = 0; i < nevents; i++) {
+        struct syspoll_resrc resrc = syspoll_get(ctx->poller, i);
+
+        if (*(int*)resrc.udata == ctx->listenfd) {  /* The request socket */
+            if (resrc.events & SYSPOLL_READ) {
+                if (!recv_and_process_requests(ctx, buf, buf_size)) {
+                    fprintf(stderr,
+                            "%s: fatal error on listen socket; exiting\n",
+                            __func__);
+                    return false;
+                }
+            }
+
+        } else {  /* One of the responses (file transfers) */
+            struct xfer* const xfer = (struct xfer*)resrc.udata;
+
+            if (!process_file_op(ctx, xfer)) {
+                /* Fatal error */
+                remove_xfer(ctx, xfer);
+            }
+        }
+    }
+
+    return true;
+}
+
+static enum prot_stat process_request(struct context* ctx,
+                                      const void* buf, const size_t size,
+                                      int* fds, const size_t nfds);
+
+static bool recv_and_process_requests(struct context* ctx,
+                                      uint8_t* buf,
+                                      const size_t buf_size)
+{
+    int recvd_fds[2] = {-1, -1};
+    size_t nfds = 2;
+
+    for (;;) {
+        const ssize_t nread = us_recv(ctx->listenfd,
+                                      buf, buf_size,
+                                      recvd_fds, &nfds);
+
+        if (nread < 0) {
+            return !errno_is_fatal(errno);
+        } else {
+            /* Recv of zero makes no sense on a UDP (connectionless) socket */
+            assert (nread > 0);
+
+            const enum prot_stat stat = process_request(ctx,
+                                                        buf, (size_t)nread,
+                                                        recvd_fds, nfds);
+            if (stat != PROT_STAT_OK) {
+                puts("REQUEST FAILED");
+                send_stat(ctx->listenfd, stat, 0, 0);
+            }
+        }
+    }
+}
+
+static enum prot_stat process_request(struct context* ctx,
+                                      const void* buf, const size_t size,
+                                      int* fds, const size_t nfds UNUSED)
+{
+    struct prot_pdu pdu;
+
+    prot_unmarshal(buf, size, &pdu);
+
+    char fname[PROT_FILENAME_MAX + 1] = {0};
+    memcpy(fname, pdu.filename, pdu.filename_len);
+    printf("XXX nrecvd: %lu; cmd: %d; stat: %d;"
+           " fnlen: %d; fname: %s;"
+           " recvd_fds[0]: %d; recvd_fds[1]: %d\n",
+           size, pdu.cmd, pdu.stat, pdu.filename_len, fname,
+           fds[0], fds[1]);
+
+    switch (pdu.cmd) {
+    case PROT_CMD_READ:
+        if (ctx->nxfers == ctx->max_xfers)
+            return PROT_STAT_XXX;
+
+        return (add_read_xfer(ctx, fname, fds[0]) ?
+                PROT_STAT_OK :
+                PROT_STAT_XXX);
+
+    case PROT_CMD_SEND:
+        if (ctx->nxfers == ctx->max_xfers)
+            return PROT_STAT_XXX;
+
+        return (add_send_xfer(ctx, fname, fds[0], fds[1]) ?
+                PROT_STAT_OK :
+                PROT_STAT_XXX);
+
+    default:
+        return PROT_STAT_XXX;
+    }
+}
+
+/* --------------- (Uninteresting) Internal implementations ------------- */
 
 static bool context_construct(struct context* ctx,
                               const int poll_timeout,
@@ -97,6 +268,59 @@ static void context_destruct(struct context* ctx)
     free(ctx->xfers);
 }
 
+static bool xfer_complete(const struct xfer* x)
+{
+    return ((size_t)file_offset(&x->file) == x->file.size);
+}
+
+static bool errno_is_fatal(const int err)
+{
+    return (err != EWOULDBLOCK && err != EAGAIN);
+}
+
+static bool process_file_op(struct context* ctx, struct xfer* xfer)
+{
+    switch (xfer->cmd) {
+    case PROT_CMD_READ:
+    case PROT_CMD_SEND: {
+        const ssize_t nwritten = file_splice(&xfer->file, xfer->dest_fd);
+
+        PRINT_XFER(xfer);
+
+        if (nwritten < 0) {
+            if (errno_is_fatal(errno)) {
+                send_xfer_stat(xfer, PROT_STAT_XXX);
+                return false;
+            }
+        } else if (nwritten == 0) {
+            return false;
+        } else {
+            /* Report the transfer via the status descriptor */
+            if (!send_xfer_stat(xfer, PROT_STAT_XFER)) {
+                if (errno_is_fatal(errno)) {
+                    /* TODO Handle fatal error */
+                }
+            }
+
+            if (xfer_complete(xfer)) {
+                remove_xfer(ctx, xfer);
+            }
+        }
+
+        return (nwritten > 0);
+    }
+
+    case PROT_CMD_STAT:
+        /* Not a file operation */
+        abort();
+
+    case PROT_CMD_CANCEL:
+        break;
+    }
+
+    return false;
+}
+
 static bool add_xfer(struct context* ctx,
                      const enum prot_cmd cmd,
                      struct file* file,
@@ -132,9 +356,9 @@ static bool add_xfer(struct context* ctx,
     return false;
 }
 
-static bool add_read(struct context* ctx,
-                     const char* filename,
-                     const int dest_fd)
+static bool add_read_xfer(struct context* ctx,
+                          const char* filename,
+                          const int dest_fd)
 {
     struct file file;
     if (!file_open_read(&file, filename))
@@ -143,10 +367,10 @@ static bool add_read(struct context* ctx,
     return add_xfer(ctx, PROT_CMD_READ, &file, dest_fd, dest_fd);
 }
 
-static bool add_send(struct context* ctx,
-                     const char* filename,
-                     const int stat_fd,
-                     const int dest_fd)
+static bool add_send_xfer(struct context* ctx,
+                          const char* filename,
+                          const int stat_fd,
+                          const int dest_fd)
 {
     struct file file;
     if (!file_open_read(&file, filename))
@@ -155,88 +379,31 @@ static bool add_send(struct context* ctx,
     return add_xfer(ctx, PROT_CMD_SEND, &file, stat_fd, dest_fd);
 }
 
-static enum prot_stat handle_request(struct context* ctx,
-                                     const void* buf, const size_t size,
-                                     int* fds, const size_t nfds UNUSED)
+static void delete_xfer(struct xfer* x)
 {
-    struct prot_pdu pdu;
-    prot_unmarshal(buf, size, &pdu);
-    char fname[PROT_FILENAME_MAX + 1] = {0};
-    memcpy(fname, pdu.filename, pdu.filename_len);
-    printf("XXX nrecvd: %lu; cmd: %d; stat: %d;"
-           " fnlen: %d; fname: %s;"
-           " recvd_fds[0]: %d; recvd_fds[1]: %d\n",
-           size, pdu.cmd, pdu.stat, pdu.filename_len, fname,
-           fds[0], fds[1]);
-
-    switch (pdu.cmd) {
-    case PROT_CMD_READ:
-        if (ctx->nxfers == ctx->max_xfers)
-            return PROT_STAT_XXX;
-
-        return (add_read(ctx, fname, fds[0]) ?
-                PROT_STAT_OK :
-                PROT_STAT_XXX);
-
-    case PROT_CMD_SEND:
-        if (ctx->nxfers == ctx->max_xfers)
-            return PROT_STAT_XXX;
-
-        return (add_send(ctx, fname, fds[0], fds[1]) ?
-                PROT_STAT_OK :
-                PROT_STAT_XXX);
-
-    default:
-        return PROT_STAT_XXX;
-    }
+    close(x->dest_fd);
+    if (x->stat_fd != x->dest_fd)
+        close(x->stat_fd);
+    file_close(&x->file);
+    free(x);
 }
 
-static bool send_error(const int fd, const enum prot_stat err)
+static bool send_stat(const int fd, const enum prot_stat stat,
+                      const size_t file_size, const off_t file_offset)
 {
     prot_stat_buf buf;
-    prot_marshal_stat(buf, sizeof(buf), err, 0, 0);
+    prot_marshal_stat(buf, sizeof(buf), stat, file_size, (size_t)file_offset);
 
-    struct iovec iov;
-    iov.iov_base = buf;
-    iov.iov_len = sizeof(buf);
+    const ssize_t n = write(fd, buf, sizeof(buf));
 
-    return (us_sendv(fd, &iov, 1, NULL, 0) > 0);
+    assert (n == -1 || n == sizeof(buf));
+
+    return (n != -1);
 }
 
-static bool read_and_process_reqs(struct context* ctx,
-                                  uint8_t* buf, const size_t buf_size)
+static bool send_xfer_stat(const struct xfer* x, const enum prot_stat stat)
 {
-    int recvd_fds[2] = {-1, -1};
-    size_t nfds = 2;
-
-    ssize_t nread;
-
-    for (;;) {
-        nread = us_recv(ctx->listenfd, buf, buf_size, recvd_fds, &nfds);
-
-        if (nread < 0) {
-            if (errno_is_fatal(errno)) {
-                /* Fatal error */
-                return false;
-            }
-            /* Non-fatal error */
-            break;
-
-        } else {
-            /* Recv of zero makes no sense on a UDP (connectionless) socket */
-            assert (nread > 0);
-
-            const enum prot_stat stat = handle_request(ctx,
-                                                       buf, (size_t)nread,
-                                                       recvd_fds, nfds);
-            if (stat != PROT_STAT_OK) {
-                puts("REQUEST FAILED");
-                send_error(ctx->listenfd, stat);
-            }
-        }
-    }
-
-    return true;
+    return send_stat(x->stat_fd, stat, x->file.size, file_offset(&x->file));
 }
 
 /*
@@ -245,7 +412,7 @@ static bool read_and_process_reqs(struct context* ctx,
   |a|b|c|d| -> |a|d|c|
 
   (i.e., the last element replaces the removed one and its index updated.)
- */
+*/
 static void remove_xfer(struct context* ctx, struct xfer* xfer)
 {
     const int i = xfer->idx;
@@ -261,118 +428,9 @@ static void remove_xfer(struct context* ctx, struct xfer* xfer)
     delete_xfer(xfer);
 }
 
-bool srv_run(const int listenfd, const char* sockname, const int maxfds)
+static void print_xfer(const struct xfer* x)
 {
-    struct context ctx;
-
-    if (!context_construct(&ctx, 1000, listenfd, sockname, maxfds))
-        return false;
-
-    if (!syspoll_register(ctx.poller,
-                          ctx.listenfd,
-                          &ctx.listenfd,
-                          SYSPOLL_READ | SYSPOLL_WRITE))
-        goto fail;
-
-    uint8_t recvbuf[PROT_PDU_MAXSIZE] = {0};
-
-    /* for (;;) { */
-    for (int i = 0; i < 3; i++) {
-        const int n = syspoll_poll(ctx.poller);
-
-        for (int j = 0; j < n; j++) {
-            struct syspoll_resrc resrc = syspoll_get(ctx.poller, j);
-
-            if (*(int*)resrc.udata == ctx.listenfd) {
-                /* The request socket */
-                if (resrc.events & SYSPOLL_READ) {
-                    if (!read_and_process_reqs(&ctx, recvbuf, sizeof(recvbuf))) {
-                        /* TODO handle fatal error */
-                    }
-                }
-
-            } else {
-                /* One of the responses (file transfers) */
-                assert (resrc.events & SYSPOLL_WRITE);
-                struct xfer* xfer = (struct xfer*)resrc.udata;
-
-                printf("XXX xfer (stat: %d; dest: %d) ready\n",
-                       xfer->stat_fd, xfer->dest_fd);
-
-                if (!process_file_op(&ctx, xfer)) {
-                    /* Fatal error */
-                    remove_xfer(&ctx, xfer);
-                }
-            }
-        }
-    }
-
-    context_destruct(&ctx);
-
-    return true;
-
- fail:
-    context_destruct(&ctx);
-    return false;
-}
-
-/* --------------- Internal implementations ------------- */
-
-static bool xfer_complete(const struct xfer* x)
-{
-    return ((size_t)file_offset(&x->file) == x->file.size);
-}
-
-static bool errno_is_fatal(const int err)
-{
-    return (err == EWOULDBLOCK || err == EAGAIN);
-}
-
-static bool process_file_op(struct context* ctx, struct xfer* xfer)
-{
-    switch (xfer->cmd) {
-    case PROT_CMD_READ:
-    case PROT_CMD_SEND: {
-        const ssize_t nwritten = file_splice(&xfer->file, xfer->dest_fd);
-
-        PRINT_XFER(xfer);
-
-        if (nwritten < 0) {
-            if (errno_is_fatal(errno)) {
-                send_error(xfer->stat_fd, PROT_STAT_XXX);
-                return false;
-            }
-        } else if (nwritten == 0) {
-            return false;
-        } else {
-            /* Report the transfer via the status descriptor */
-            prot_stat_buf buf;
-
-            prot_marshal_stat(buf, sizeof(buf), PROT_STAT_XFER,
-                              xfer->file.size,
-                              (uint64_t)file_offset(&xfer->file));
-
-            if (write(xfer->stat_fd, buf, sizeof(buf)) != sizeof(buf) == -1) {
-                if (errno_is_fatal(errno)) {
-                    /* TODO Handle fatal error */
-                }
-            }
-
-            if (xfer_complete(xfer)) {
-                remove_xfer(ctx, xfer);
-            }
-        }
-
-        return (nwritten > 0);
-    }
-
-    case PROT_CMD_STAT:
-        /* Not a file operation */
-        abort();
-
-    case PROT_CMD_CANCEL:
-        break;
-    }
-
-    return false;
+    printf("stat_fd: %d; dest_fd: %d; cmd: %d;"
+           " file->size: %ld; file->blksize: %d\n",
+           x->stat_fd, x->dest_fd, x->cmd, x->file.size, x->file.blksize);
 }
