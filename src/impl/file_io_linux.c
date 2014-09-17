ca6b5ae021c79af5d3dda45ce1ca2c9a5ac7dc0a
fk <francoisk777@gmail.com>
2014-09-17 09:02:16 +0200

    Removed commented-out code.

--- src/impl/file_io_linux.c
+++ src/impl/file_io_linux.c
@@ -4,22 +4,6 @@
 
 #include "file_io.h"
 
-/* ssize_t file_splice(struct file* file, int fd) */
-/* { */
-/*     loff_t file_off = file_offset(file); */
-
-/*     printf("CCC file_off: %ld; blksize: %d\n", file_off, file->blksize); */
-
-/*     const ssize_t n = splice(fileno(file->ptr), &file_off, */
-/*                              fd, NULL, */
-/*                              (size_t)file->blksize, */
-/*                              0); */
-
-/*     printf("DDD file_off: %ld; blksize: %d\n", file_off, file->blksize); */
-
-/*     return n; */
-/* } */
-
 ssize_t file_splice(struct file* file, int fd)
 {
     return splice(fileno(file->ptr), NULL,
