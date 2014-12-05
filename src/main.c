5d1644324aa8d179c754949172f2af14105b4566
fk <francoisk777@gmail.com>
2014-12-05 11:55:47 +0200

    If the program is invoked with a root directory of "/" it will not chroot and therefore root privileges (invoked by root user or setuid bit set on executable) are not required in these cases.

--- src/main.c
+++ src/main.c
@@ -51,7 +51,7 @@
 
 static const long OPEN_FD_TIMEOUT_MS_MAX = 60 * 60 * 1000;
 
-static void print_usage(const char* progname, long fd_timeout_ms);
+static void print_usage(long fd_timeout_ms);
 static bool sync_parent(int status_code);
 static long opt_strtol(const char*);
 static bool chroot_and_drop_privs(const char* root_dir,
@@ -117,13 +117,13 @@ int main(const int argc, char** argv)
             return EXIT_FAILURE;
 
         default:
-            print_usage(argv[0], fd_timeout_ms);
+            print_usage(fd_timeout_ms);
             return EXIT_FAILURE;
         }
     }
 
     if (!root_dir || !srvname || (maxfiles == 0)) {
-        print_usage(argv[0], fd_timeout_ms);
+        print_usage(fd_timeout_ms);
         return EXIT_FAILURE;
     }
 
@@ -167,24 +167,24 @@ int main(const int argc, char** argv)
     if (do_sync) {
         if (!sync_parent(0)) {
             LOGERRNO("Failed to sync with parent");
-            return EXIT_FAILURE;
+            goto fail1;
         }
         close(PROC_SYNCFD);
     }
 
     if (daemonise && !proc_daemonise(&requestfd, 1))
-        return EXIT_FAILURE;
+        goto fail1;
 
     openlog(FIOD_PROGNAME, LOG_NDELAY | LOG_CONS | LOG_PID, LOG_DAEMON);
 
     if (!chroot_and_drop_privs(root_dir, new_uid, new_gid)) {
         us_stop_serving(srvname, requestfd);
-        return EXIT_FAILURE;
+        goto fail1;
     }
 
     syslog(LOG_INFO,
-           "Starting; name: %s; root_dir: %s;"
-           " uid: %d (%s); gid: %d (%s);"
+           "Starting; name: %s; root_dir: \"%s\";"
+           " uid: %d %s; gid: %d %s;"
            " maxfiles: %ld; fd_timeout_ms: %ld\n",
            srvname, root_dir,
            getuid(), uname, getgid(), gname, maxfiles, fd_timeout_ms);
@@ -192,7 +192,8 @@ int main(const int argc, char** argv)
     const bool success = srv_run(requestfd, (int)maxfiles, fd_timeout_ms);
 
     if (!success) {
-        syslog(LOG_EMERG, "srv_run() failed [%s]; server shutting down\n",
+        syslog(LOG_EMERG,
+               "srv_run() failed [%s]; server shutting down\n",
                strerror(errno));
     } else {
         syslog(LOG_INFO, "Shutting down\n");
@@ -200,9 +201,12 @@ int main(const int argc, char** argv)
 
     us_stop_serving(srvname, requestfd);
 
-    closelog();
-
     return (success ? EXIT_SUCCESS : EXIT_FAILURE);
+
+ fail1:
+    us_stop_serving(srvname, requestfd);
+
+    return EXIT_FAILURE;
 }
 
 static bool chroot_and_drop_privs(const char* root_dir,
@@ -222,19 +226,24 @@ static bool chroot_and_drop_privs(const char* root_dir,
         return false;
     }
 
-    if (euid != 0) {
-        syslog(LOG_ERR, "Executable doesn't appear to be setuid\n");
-        return false;
-    }
+    if (strncmp(root_dir, "/", 2) != 0) {
+        if (euid != 0) {
+            syslog(LOG_ERR, "Executable doesn't appear to be setuid\n");
+            return false;
+        }
 
-    if (chroot(root_dir) == -1) {
-        syslog(LOG_ERR, "Couldn't chroot to %s\n", root_dir);
-        return false;
-    }
+        if (chroot(root_dir) == -1) {
+            syslog(LOG_ERR, "Couldn't chroot to %s\n", root_dir);
+            return false;
+        }
 
-    if (chdir("/") == -1) {
-        syslog(LOG_ERR, "Couldn't chdir to '/'\n");
-        return false;
+        if (chdir("/") == -1) {
+            syslog(LOG_ERR, "Couldn't chdir to '/'\n");
+            return false;
+        }
+    } else {
+        syslog(LOG_WARNING,
+               "Not chrooting because user-specified root dir is \"/\"\n");
     }
 
     if (new_gid != gid && setgid(new_gid) == -1) {
@@ -266,10 +275,11 @@ static long opt_strtol(const char* s)
     }
 }
 
-static void print_usage(const char* progname, const long fd_timeout_ms)
+static void print_usage(const long fd_timeout_ms)
 {
-    printf("Usage:\n"
-           "%s\n"
+    printf("Usage: "
+           FIOD_PROGNAME" OPTION\n"
+           "\nOptions:\n"
            "-r <root_dir> (chroot to this directory)\n"
            "-s <server_name> (user-friendly name to identify server instance)\n"
            "-n <maxfiles> (maximum number of concurrent file transfers)\n"
@@ -277,7 +287,7 @@ static void print_usage(const char* progname, const long fd_timeout_ms)
            "[-g <group_name>] (run as different group)\n"
            "[-p (sync with parent process (via a pipe))]\n"
            "[-t <open_fd_timeout_ms> (default: %ld)]\n",
-           progname, fd_timeout_ms);
+           fd_timeout_ms);
 }
 
 static bool sync_parent(const int status)
